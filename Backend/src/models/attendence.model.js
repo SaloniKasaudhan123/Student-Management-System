456117f70dea6b4b997d40e030ce1fb4b5df0336
Saloni <saloni1404999@gmail.com>
2026-06-22 10:50:08 +0530

    Added attendence model

---
+++ Backend/src/models/attendence.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const attendenceSchema = new mongoose.Schema({
+    student:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Student",
+        required:true
+    },
+    date:{
+        type:Date,
+        required:[true, "Date is required"],
+    },
+    status:{
+        type:String,
+        required:true,
+        enum:["Present","Absent"]
+    }
+} , {timestamps:true});
+
+export const Attendence = mongoose.model("Attendence",attendenceSchema);
